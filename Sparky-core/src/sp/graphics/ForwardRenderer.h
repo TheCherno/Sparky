612f7cae436a8a1918ce4d27a0bf8f4a68028674
TheCherno <cherno21@hotmail.com>
2016-01-19 14:22:30 +1100

    Added Lights and LightSetups (WIP). Test3D now contains a little light demo.

--- Sparky-core/src/sp/graphics/ForwardRenderer.h
+++ Sparky-core/src/sp/graphics/ForwardRenderer.h
@@ -15,10 +15,12 @@ namespace sp { namespace graphics {
 		void Begin() override;
 		void Submit(const RenderCommand& command) override;
 		void SubmitMesh(Camera* camera, Mesh* mesh, const maths::mat4& transform) override;
+		void SubmitLightSetup(const LightSetup& lightSetup);
 		void End() override;
 		void Present() override;
 	private:
 		void SetRequiredUniforms(Shader* shader, const std::vector<RendererUniform>& uniforms);
+		void SetSystemUniforms(Shader* shader);
 	};
 
 } }
---
+++ Sparky-core/src/sp/graphics/Light.cpp
@@ -0,0 +1,8 @@
+#include "sp/sp.h"
+#include "LightSetup.h"
+
+namespace sp { namespace graphics {
+
+
+
+} }
