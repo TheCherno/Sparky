853593df75902b72b836ab44f3e052bd19870056
Lukáš Jech <klukule@moowdesign.eu>
2016-04-04 18:56:22 +0200

    Added sponza scene

---
+++ Sandbox/src/Sponza.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Sparky.h>
+
+class Sponza : public sp::graphics::Layer3D
+{
+private:
+	sp::graphics::Camera* m_MayaCamera;
+	sp::graphics::Camera* m_FPSCamera;
+
+	std::vector<sp::graphics::PBRMaterial*> m_Materials;
+	std::vector<sp::entity::Entity*> m_Entities;
+	sp::graphics::MaterialInstance* m_SkyboxMaterial;
+
+public:
+	Sponza();
+	~Sponza();
+
+	void OnInit(sp::graphics::Renderer3D& renderer, sp::graphics::Scene& scene) override;
+
+	void OnTick() override;
+	void OnUpdate() override;
+	void OnEvent(sp::events::Event& event) override;
+
+	void OnRender(sp::graphics::Renderer3D& renderer);
+};
