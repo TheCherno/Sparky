1a7f9d4360df7acf44e9022ce534276f69da1f47
TheCherno <cherno21@hotmail.com>
2016-04-18 13:49:16 +1000

    Visual Studio 2015 Update 2 fixes.

--- Sparky-core/src/sp/system/Memory.h
+++ Sparky-core/src/sp/system/Memory.h
@@ -5,42 +5,11 @@
 #define spnew		new(__FILE__, __LINE__)
 #define spdel		delete
 
-inline void* operator new(size_t size)
-{
-	return sp::Allocator::Allocate(size);
-}
-
-inline void* operator new(size_t size, const char* file, uint line)
-{
-	return sp::Allocator::AllocateDebug(size, file, line);
-}
-
-inline void* operator new[](size_t size)
-{
-	return sp::Allocator::Allocate(size);
-}
-
-inline void* operator new[](size_t size, const char* file, uint line)
-{
-	return sp::Allocator::AllocateDebug(size, file, line);
-}
-
-inline void operator delete(void* block)
-{
-	sp::Allocator::Free(block);
-}
-
-inline void operator delete(void* block, const char* file, uint line)
-{
-	sp::Allocator::FreeDebug(block, file, line);
-}
-
-inline void operator delete[](void* block)
-{
-	sp::Allocator::Free(block);
-}
-
-inline void operator delete[](void* block, const char* file, uint line)
-{
-	sp::Allocator::FreeDebug(block, file, line);
-}
+void* operator new(size_t size);
+SP_API void* operator new(size_t size, const char* file, uint line);
+void* operator new[](size_t size);
+SP_API void* operator new[](size_t size, const char* file, uint line);
+void operator delete(void* block);
+SP_API void operator delete(void* block, const char* file, uint line);
+void operator delete[](void* block);
+SP_API void operator delete[](void* block, const char* file, uint line);
