9a1e6aa011b27bd6fd40bee30a550f716b110860
Jacob-Mango <mangosprograms@gmail.com>
2016-05-03 23:11:21 +1000

    Another commit.

---
+++ Sandbox/src/Sandbox.cpp
@@ -0,0 +1,43 @@
+#include <Sparky.h>
+
+#include "Test2D.h"
+#include "Test3D.h"
+#include "DeferredTest.h"
+#include "SponzaTest.h"
+
+using namespace sp;
+using namespace graphics;
+using namespace maths;
+
+class Game : public Application
+{
+public:
+	Game()
+		: Application("Sandbox", { 1280, 720, false, false }, sp::graphics::API::RenderAPI::DIRECT3D)
+	{
+	}
+
+	~Game()
+	{
+	}
+
+	void Init() override
+	{
+		Application::Init();
+		VFS::Get()->Mount("models", "res/models");
+		VFS::Get()->Mount("pbr", "res/pbr");
+		VFS::Get()->Mount("shaders", "shaders");
+
+		// PushLayer(new Test2D());
+		PushLayer(new Test3D());
+		// PushLayer(new SponzaTest());
+		// PushLayer(new DeferredTest()); // Doesn't work atm
+	}
+};
+
+int main()
+{
+	Game game;
+	game.Start();
+	return 0;
+}
